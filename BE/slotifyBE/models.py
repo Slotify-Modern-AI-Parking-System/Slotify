0d3d2b0cb2e56ea6120a7cfbb945d87c2cff21ef
Doshijainam <Jdoshi3117@conestogac.on.ca>
2025-01-17 11:15:21 -0500

    Owner Register FE Page.

---
+++ BE/slotifyBE/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class OwnerProfile(models.Model):
+    firstName = models.CharField(max_length=50, null=False)
+    lastName = models.CharField(max_length=50, null=False)
+    emailId = models.EmailField(max_length=255, unique=True, null=False)
+    password = models.CharField(max_length=255, null=False)
+    contactNumber = models.CharField(max_length=10, unique=True, null=False)
+    idProof = models.URLField(max_length=500, blank=True, null=True)
