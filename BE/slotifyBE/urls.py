c5d35001857c28f534b9ed6456bc30996eed4abc
Jainam Doshi <jainamdoshi@Jainams-MacBook-Pro.local>
2025-05-21 18:49:16 -0400

    Merge Conflict Resolution 1

--- BE/slotifyBE/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('userRegister/', views.userRegister, name='userRegister'),
-    path('registerOwner/', views.register_owner, name='register_owner'),
-]
