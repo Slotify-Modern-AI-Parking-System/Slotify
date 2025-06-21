ea48294143ea6bb606037c63ffa9146ab2b5117b
jigs074 <jigspatel7624@gmail.com>
2025-06-20 21:32:42 -0400

    Made minor Fix

--- BE/User/urls.py
+++ BE/User/urls.py
@@ -6,18 +6,6 @@ urlpatterns = [
     # Main dashboard
     path('entry/', views.index, name='index'),
     path('welcome/', views.welcome,name="WelcomePage"),
-    
-    # Detection control endpoints
-    path('start/', views.start_detection, name='start_detection'),
-    path('stop/', views.stop_detection, name='stop_detection'),
-    
-    # Plate detection endpoints
-    path('plate-detected/', views.plate_detected, name='plate_detected'),
-    path('confirm-plate/', views.confirm_plate, name='confirm_plate'),
-    
-    # Status endpoints
-    path('status/', views.get_status, name='get_status'),
-    path('stream/', views.detection_stream, name='detection_stream'),
     path('parking-lot/login/', views.parking_lot_login, name="ParkingLotLogin"),
     path('parking-lot/logout/', views.parking_lot_logout, name="ParkingLotLogout"),
 
